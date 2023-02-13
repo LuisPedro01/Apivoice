3c7c9ed12438db1622cb50df294f0b8885a18d94
a42597 <a42597@alunos.ipb.pt>
2023-02-13 17:15:45 +0000

    base dos comandos por voz

--- App.js
+++ App.js
@@ -5,50 +5,85 @@ import {
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
-  NativeModules
-} from 'react-native';
-import { useEffect, useState } from "react"
-import axios from "axios";
+  NativeModules,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { firebase } from "./src/services/firebase";
 
 export default function App() {
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
+  const [userDoc, setUserDoc] = useState([]);
+  const ApiRef = firebase.firestore().collection("apiarios");
+
+  const getDadosApi = () => {
+    ApiRef.onSnapshot((querySnapshot) => {
+      const userDoc = [];
+      querySnapshot.forEach((doc) => {
+        const { localizacao, nome } = doc.data();
+        userDoc.push({
+          id: doc.id,
+          localizacao,
+          nome,
+        });
+      });
+      setUserDoc(userDoc);
+    });
+  };
 
   useEffect(() => {
-    // axios.get('http://127.0.0.1:5000/data')
-    // .then(
-    //   res => setData(res.data),
-    //   console.log("valor",data)
-    // )
-    // .catch(error => console.log("error",error))
+    getDadosApi();
+  }, []);
 
-    fetch("http://192.168.1.72:3000/", {
-      method: 'GET',
+  useEffect(() => {
+    fetch("http://192.168.1.106:3000", {
+      method: "GET",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
+        Accept: "application/json, text/plain",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.text())
+      .then((data) => {
+        setData(data);
+        console.log("data", data);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  if (`selecionar apiário ${userDoc.nome}` in data) {
+    useNavigation.navigate("Home", {
+      nomeApi: item,
+    });
+    if (`selecionar colmeia ${userDoc.nome}` in data) {
+      useNavigation.navigate("Colmeia", {
+        nomeApi: item,
+      });
     }
-    )
-    .then(resp=>resp.json())
-    .then(
-      data => {
-        setData(data)
-        console.log("data",data)
-      }
-    ).catch(error => console.log("error", error))
-  }, [])
+  }
+  if (`reproduzir ultima gravaççao` in data) {
+    //reproduz gravação
+  }
+  if (`Criar apiario` in data) {
+    useNavigation.navigate("Novo Apiario");
+  }
+  if (`Criar colmeia` in data) {
+    useNavigation.navigate("Nova Colmeia");
+  }
+  if (`Gravar` in data) {
+    useNavigation.navigate("Audio Recorder");
+  }
 
   return (
     <SafeAreaView style={styles.root}>
       <Navigation />
     </SafeAreaView>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: '#F9FBFC'
-  }
-})
+    backgroundColor: "#F9FBFC",
+  },
+});
---
+++ src/Screens/server.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect, jsonify
+import speech_recognition as sr
+import os 
+
+app = Flask(__name__)
+
+#Members API Route
+
+@app.route("/", methods=["GET", "POST"])
+def voice_command():
+    #data = {"key1": "value1", "key2": "value2"}
+    #return jsonify({"Hello":"World"})
+    comando=ouvir_microfone()
+    return(comando)
+
+
+
+#função para ouvir e reconhecer a fala
+def ouvir_microfone():
+    #Habilita o microfone do usuario 
+    microfone= sr.Recognizer()
+
+    #usando o microfone
+    with sr.Microphone() as source:
+
+        #chama um algoritmo de redução de ruidos de som 
+        microfone.adjust_for_ambient_noise(source)
+
+        #Frase para o ususario dizer algo
+        print("Diga alguma coisa: ")
+
+        #Armazena o que foi dito numa variavel
+        audio=microfone.listen(source)
+
+    try:
+        #Passa a variavel para o algoritmo reconhecedor de padroes
+        frase= microfone.recognize_google(audio,language='pt-PT')
+
+        if "selecionar apiário" in frase:
+            os.system("start Chrome.exe")
+
+        if "Adicionar colmeia" in frase:
+            os.system("start Chrome.exe")
+
+        if "Abrir apiário" in frase:
+            os.system("start Chrome.exe")
+
+        if "Abrir colmeia" in frase:
+            os.system("start Chrome.exe")
+ 
+        if "Gravar audio" in frase:
+            os.system("start Chrome.exe")
+
+        if "Reproduzir ultima gravação" in frase:
+            os.system("start Chrome.exe")
+
+        if "Retroceder" in frase:
+            os.system("start Chrome.exe")
+
+        #Retorna a frase pronunciada
+        print("Voce disse: " + frase)
+        return frase
+    
+    #Se não reconheceu o padrão de fala, exiba a mensagem
+    except sr.UnknownValueError:
+        print("Não entendi o seu comando")
+        return sr.UnknownValueError
+
+if __name__ == "__main__":
+    app.run(host='192.168.1.106', port=3000,debug=True)
+
