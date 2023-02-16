e09db58698656a706e9df820a536021dd533e7ca
Luis Pedro Carvalho Rodrigues <a42597@alunos.ipb.pt>
2023-02-16 19:15:13 +0000

    .2

--- src/Screens/AlterarApiario.js
+++ src/Screens/AlterarApiario.js
@@ -42,7 +42,6 @@ export default function AlterarApiario({route}) {
   }
 
   const novoApi = (item) => {
-    console.log(item.nome)
     if(nomeApi != item.nome){
       setButtonValue(item.nome)
     }
